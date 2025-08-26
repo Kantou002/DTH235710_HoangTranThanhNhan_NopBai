9af9596e8ab0433edce5c8492b8f0a80b203e003
DTH235710_HoangTranThanhNhan <crazyldg113@gmail.com>
2025-08-26 09:18:15 +0700

    Add files via upload

---
+++ Chuong1.1.py
@@ -0,0 +1,15 @@
+#Chuong Trinh tinh tong 2 so
+
+#Nhap 2 so tu ban phim
+a = int(input("Nhap so a: "))
+b = int(input("Nhap so b: "))
+#Tinh tong 2 so
+tong = a + b
+#In ket qua
+print("Tong cua 2 so la: ", tong)
+
+
+
+
+
+
